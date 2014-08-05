e7cb4b914e0b6d982a056692f18d5c9fd723920a
Anthony Wong <awmwong@gmail.com>
2014-08-05 17:01:16 -0400

    Pods

---
+++ Example/Pods/Pods-KikAPI-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KikAPI
+#define COCOAPODS_POD_AVAILABLE_KikAPI
+#define COCOAPODS_VERSION_MAJOR_KikAPI 0
+#define COCOAPODS_VERSION_MINOR_KikAPI 1
+#define COCOAPODS_VERSION_PATCH_KikAPI 0
+
---
+++ Example/Pods/Pods-Tests-KikAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tests_KikAPI : NSObject
+@end
+@implementation PodsDummy_Pods_Tests_KikAPI
+@end
